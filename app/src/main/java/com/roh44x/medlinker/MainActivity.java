5c1df45fb38b4cea00c2b3e8fd7f578aa89660cc
Serban Jinga <mihaiserbanjinga@gmail.com>
2019-06-08 11:30:51 +0300

    Second Commit

--- app/src/main/java/com/roh44x/medlinker/MainActivity.java
+++ app/src/main/java/com/roh44x/medlinker/MainActivity.java
@@ -2,9 +2,11 @@ package com.roh44x.medlinker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
