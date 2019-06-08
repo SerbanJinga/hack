f7b714bcde7d4aca6a585c392c12d23a08a202c0
tiberiuureche <tiberiu.ureche@outlook.com>
2019-06-08 12:13:26 +0300

    Register System implemented.

--- app/src/main/java/com/roh44x/medlinker/MainActivity.java
+++ app/src/main/java/com/roh44x/medlinker/MainActivity.java
@@ -1,18 +1,137 @@
 package com.roh44x.medlinker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.roh44x.medlinker.R;
+import com.roh44x.medlinker.components.User;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    FirebaseAuth mAuth;
+    public FirebaseAuth mAuth;
+    public FirebaseDatabase mDatabase;
+    public DatabaseReference userDatabase;
+
+    public EditText etFirstName, etLastName, etEmail, etPassword, etRepeatPassword, etAge;
+    public Button btnSignUp, btnHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFirebase();
+
+        etFirstName = (EditText)findViewById(R.id.etFirstName);
+        etLastName = (EditText)findViewById(R.id.etLastName);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        etRepeatPassword = (EditText)findViewById(R.id.etRepeatPass);
+        etAge = (EditText)findViewById(R.id.etAge);
+        btnSignUp = (Button)findViewById(R.id.btnRegister);
+        btnHaveAccount = (Button)findViewById(R.id.btnAlreadyAcc);
+
+        btnSignUp.setOnClickListener(this);
+        btnHaveAccount.setOnClickListener(this);
     }
+
+    public void initFirebase() {
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        userDatabase = mDatabase.getReference();
+    }
+
+    public void signUpWithEmailAndPassword(String email, String password) {
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter email",Toast.LENGTH_SHORT).show();
+            etEmail.startAnimation(shakeError());
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password",Toast.LENGTH_SHORT).show();
+            etPassword.startAnimation(shakeError());
+            return;
+        }
+        if(TextUtils.isEmpty(etFirstName.getText().toString())){
+            Toast.makeText(this, "Please enter your First Name",Toast.LENGTH_SHORT).show();
+            etFirstName.startAnimation(shakeError());
+            return;
+        }
+        if(TextUtils.isEmpty(etLastName.getText().toString())){
+            Toast.makeText(this, "Please enter your Second Name",Toast.LENGTH_SHORT).show();
+            etLastName.startAnimation(shakeError());
+            return;
+        }
+        if(TextUtils.isEmpty(etRepeatPassword.getText().toString())){
+            Toast.makeText(this, "Please repeat your password",Toast.LENGTH_SHORT).show();
+            etRepeatPassword.startAnimation(shakeError());
+            return;
+        }
+        if(!etPassword.getText().toString().equals(etRepeatPassword.getText().toString())){
+            Toast.makeText(this, "Passwords don't match",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            storeUserInDatabase();
+                        } else {
+                            String error = task.getException().getMessage();
+                            Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void storeUserInDatabase() {
+        final User user = new User(etFirstName.getText().toString(), etLastName.getText().toString(), etEmail.getText().toString(), null, Integer.parseInt(etAge.getText().toString()));
+
+        userDatabase.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(user).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                } else {
+                    String error = task.getException().getMessage();
+                    Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public TranslateAnimation shakeError(){
+        TranslateAnimation shake = new TranslateAnimation(0, 10, 0, 0);
+        shake.setDuration(500);
+        shake.setInterpolator(new CycleInterpolator(7));
+        return shake;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if(i == R.id.btnRegister) {
+            signUpWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString());
+        } else if(i == R.id.btnAlreadyAcc){
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+        }
+    }
+
+
 }
