be2041251d06ae910b5054e5bcf9070e46fe57ad
tiberiuureche <tiberiu.ureche@outlook.com>
2019-06-08 12:12:45 +0300

    User arhitecture added.

---
+++ app/src/main/java/com/roh44x/medlinker/components/User.java
@@ -0,0 +1,20 @@
+package com.roh44x.medlinker.components;
+
+public class User {
+
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String disease;
+    public Integer age;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, String disease, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.disease = disease;
+        this.age = age;
+    }
+}
